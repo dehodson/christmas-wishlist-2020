ce961712d26a8d7827c476bd902543e05bb5c7e2
Drew Hodson <dehodson@gmail.com>
2020-11-27 16:00:09 -0500

    Work on adding overwriting records to input.hs

--- input.hs
+++ input.hs
@@ -1,12 +1,14 @@
 import qualified Data.ByteString.Lazy as B
 import Questionnaire
 import GHC.Generics
+import Data.Maybe
 import Data.Aeson
 import System.IO
+import Data.List
 
 sectionNames :: [String]
 sectionNames = [ "Favorite Drinks"
-               , "Favorite Candy"
+               , "Favorite Candy &amp; Sweets"
                , "Favorite Salty Snacks"
                , "Your Obsession"
                , "Favorite Movies"
@@ -43,8 +45,20 @@ getQuestionnaire name = do
   sections <- sequenceA $ getSection <$> sectionNames
   return $ Questionnaire name sections
 
+insertQuestionnaire :: Questionnaire -> [Questionnaire] -> [Questionnaire]
+insertQuestionnaire q qs = do
+  let i = findIndex (((name q) ==) . name) qs
+  case i of
+    Just index -> take (index - 1) qs ++ [q] ++ drop (index + 1) qs
+    Nothing -> q : qs
+
+mergeQuestionnaires :: [Questionnaire] -> [Questionnaire] -> [Questionnaire]
+mergeQuestionnaires = foldr insertQuestionnaire
+
 main :: IO ()
 main = do
+  contents <- decodeFileStrict "questionnaires.json" :: IO (Maybe [Questionnaire])
+  let initialQuestionnaires = fromMaybe [] contents
   let loop = do {
     name <- promptFor "Person's name: " ;
     case name of
@@ -55,5 +69,5 @@ main = do
       }
   }
   questionnaires <- loop
-  encodeFile "questionnaires.json" questionnaires
+  encodeFile "questionnaires.json" (mergeQuestionnaires questionnaires initialQuestionnaires)
   putStrLn "Thank you! :)"
